40c9f5ef06230b1f546c31b34276c60ccbb5717d
Bahaagac <48641679+Bahaagac@users.noreply.github.com>
2020-10-23 06:50:36 +0300

    Single Page Application

--- reactapp/src/components/MainComponent.js
+++ reactapp/src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
-import {DISHES} from '../shared/dishes';
-import DishDetail from './DishDetailComponent';
+import Contact from './ContactComponent';
 import Header from '../components/HeaderComponent';
 import Footer from '../components/FooterComponent';
+import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
+import DishDetail from './DishDetailComponent';
 import { Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -14,6 +18,9 @@ class Main extends Component {
 
     this.state = {
       dishes : DISHES,
+      COMMENTS : COMMENTS,
+      promotions : PROMOTIONS,
+      leaders : LEADERS
       
     };
   }
@@ -22,18 +29,18 @@ class Main extends Component {
 
   render() {
 
-        const HomePage = () => {
-          return (
-            <Home/>
-          );
-        }
+        
          
           return (
             <div>
               <Header/>
               <Switch>
-                <Route path="/home" component = {HomePage} />
+                <Route path="/home" component = {() => <Home dish= {this.state.dishes.filter((dish) => dish.featured)[0]} 
+                  promotion= {this.state.promotions.filter((promo) => promo.featured)[0]} 
+                  leader= {this.state.leaders.filter((leader) => leader.featured)[0]} 
+                  /> } />
                 <Route exact path="/menu" component = {() => <Menu dishes= {this.state.dishes}/>} />
+                <Route exact path="/contactus" component = {Contact} />
                 <Redirect to ="/home" />
               </Switch>              
               <Footer/>
